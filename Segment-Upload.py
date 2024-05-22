49c57e0cb64bb680ad4e26ceccc5511870a33949
Nikhil-Lakha <61081198+Nikhil-Lakha@users.noreply.github.com>
2024-05-22 16:46:57 +0200

    Update Segment-Upload.py

--- Segment-Upload.py
+++ Segment-Upload.py
@@ -58,7 +58,7 @@ def main():
         # Prepare files for upload
         output = io.StringIO()
         st.write(output)
-        df.to_csv(output, sep="\t", index=False, header=False, quoting=csv.QUOTE_NONE, escapechar="")
+        df.to_csv(output, sep="\t", index=False, header=False, quoting=csv.QUOTE_NONE, escapechar="\t")
         txt_content = output.getvalue()
 
         files = {
