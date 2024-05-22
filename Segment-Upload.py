2fa9ecd666cb617a1f8eaeed6633661bf4471cb3
Nikhil-Lakha <61081198+Nikhil-Lakha@users.noreply.github.com>
2024-05-22 15:01:12 +0200

    Update Segment-Upload.py
    Adding Escape Character

--- Segment-Upload.py
+++ Segment-Upload.py
@@ -56,7 +56,7 @@ def main():
         
         # Prepare files for upload
         output = io.StringIO()
-        df.to_csv(output, sep="\t", index=False, header=False, quoting=csv.QUOTE_NONE)
+        df.to_csv(output, sep="\t", index=False, header=False, quoting=csv.QUOTE_NONE, escapechar=",")
         txt_content = output.getvalue()
 
         files = {
